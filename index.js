69f67bdfbbe3366d26633bf7d0e0f441b1e432b5
nitin <nitin@adit.com>
2018-08-28 11:23:22 +0530

    added numbers for live

--- index.js
+++ index.js
@@ -1,97 +1,82 @@
-  var tagSearchForArray = ['a', 'p'];
-    var attrInfo = document.querySelectorAll("a[href^='tel:']");
-    var elementsForSearch;
-    var pattern = /\D?[0-9]{3}\D?\s?[0-9]{3}\D?\s?[0-9]{4}/g;
-    var n = [];
-    for (var i = 0; i < tagSearchForArray.length; i++) {
-      elementsForSearch = document.getElementsByTagName(tagSearchForArray[i]);
-      for (var j = 0; j < elementsForSearch.length; j++) {
-        if (elementsForSearch[j].innerText.toLowerCase().match(pattern) != null) {
-          for (let value of elementsForSearch[j].innerText.toLowerCase().match(pattern)) {
-            // value = value.trim().replace(/[^\d\+]/g,"");
-            value = value.trim();
-            if (n.indexOf(value) == -1) {
-              n.push(value);
-            }
+var tagSearchForArray = ['a', 'p'];
+  var attrInfo = document.querySelectorAll("a[href^='tel:']");
+  var elementsForSearch;
+  var pattern = /(?:\d{1}\s)?\(?(\d{3})\)?-?\s?(\d{3})-?\s?(\d{4})/g;
+  var n = [];
+  var source = '';
+
+  //  For innerHTML Content
+  for (var i = 0; i < tagSearchForArray.length; i++) {
+    elementsForSearch = document.getElementsByTagName(tagSearchForArray[i]);
+    for (var j = 0; j < elementsForSearch.length; j++) {
+      if (elementsForSearch[j].innerText.toLowerCase().match(pattern) != null) {
+        for (let value of elementsForSearch[j].innerText.toLowerCase().match(pattern)) {
+          value = value.trim();
+          if (n.indexOf(value) == -1) {
+            n.push(value);
           }
         }
       }
     }
+  }
 
-    for(let key in attrInfo){
-      if(typeof attrInfo[key].href != "undefined" && attrInfo[key].href != "undefined"){
-        var ret = attrInfo[key].href.replace('tel:','');
-        // ret = ret.trim().replace(/[^\d\+]/g,"");
-        ret = ret.trim();
-        if(n.indexOf(ret) === -1){
-          n.push(ret);
+  //  For href attr param
+  for(let key in attrInfo){
+    if(typeof attrInfo[key].href != "undefined" && attrInfo[key].href != "undefined"){
+      var ret = attrInfo[key].href.replace('tel:','');
+      ret = ret.trim();
+      if(n.indexOf(ret) === -1){
+        n.push(ret);
+      }
+    }
+  }
+
+  changeNumberOnDom = function(replaceNumberArr){
+    for(let key in replaceNumberArr) {
+      var swap_targets = replaceNumberArr[key].swap_targets[0];
+      var trackingNumber = replaceNumberArr[key].number.national;
+      for (var i = 0; i < n.length; i++) {
+        let value = removeSpecialCharFromNumber(n[i]);
+        if (value == swap_targets) {
+          document.body.innerHTML = document.body.innerHTML.split(n[i]).join(trackingNumber);
         }
       }
     }
+  }
 
-    function changeNumberOnDom(replaceNumberArr){
-        for(let key in replaceNumberArr){
-            var swap_targets = replaceNumberArr[key].swap_targets[0];
-            var trackingNumber = replaceNumberArr[key].number.national;
-            // var trackingNumber = replaceNumberArr[key].number.national_string;
-            // console.log("swap_targets::",swap_targets);
-            // console.log("trackingNumber::",trackingNumber);
-            // document.body.innerHTML = document.body.innerHTML.split(key).join(replaceNumberArr[key]);
-            document.body.innerHTML = document.body.innerHTML.split(swap_targets).join(trackingNumber);
-        }
+  getSource = function(e, a) {
+    return e = e || "direct",
+    e.match(/doubleclick/) || a.match(/gclid=/) ? "google_paid" : e.match(/google/) && !e.match(/mail\.google\.com/) ? e.match(/maps\.google\.[a-z\.]{2,5}/) ? "google_local" : e.match(/google\.[a-z\.]{2,5}\/(aclk|afs)/) || e.match(/googleadservices/) || a.match(/utm_(medium|source)=[cp]pc/i) || a.match(/(matchtype|adposition)=/i) ? "google_paid" : "google_organic" : e.match(/yahoo/) && !e.match(/mail\.yahoo\.com/) ? e.match(/local\.(search\.)?yahoo\.com/) ? "yahoo_local" : a.match(/utm_medium=[cp]pc/i) ? "yahoo_paid" : "yahoo_organic" : e.match(/(\/|\.)bing\./) || a.match(/utm_source=bing/i) ? e.match(/bing\.com\/local/) ? "bing_local" : a.match(/utm_medium=[pc]pc/i) ? "bing_paid" : "bing_organic" : e.match(/msn\.com/) ? "bing_paid" : "direct" === e ? a.match(/utm_medium=[cp]pc/i) && a.match(/utm_source=google/i) ? "google_paid" : "direct" : CallTrkSwapAdit.getReferrerDomain(e)
+  }
+
+  getReferrerDomain = function(e, a) {
+    var a = e.split("/")[2]
+    , r = a.split(".");
+    return r.length > 2 ? r[r.length - 2] + "." + r[r.length - 1] : a
+  }
+
+  getFilterNumberArray = function(n) {
+    let number = [];
+    for (var i = 0; i < n.length; i++) {
+      let value = removeSpecialCharFromNumber(n[i]);
+      if(number.indexOf(value) === -1){
+        number.push(value);
+      }
     }
-    
-    
-    function loadDoc() {
-      var xhttp = new XMLHttpRequest();
-      // xhttp.open("GET", "http://localhost:1337/number/swap?number="+n, true);
-      xhttp.open("GET", "http://18.208.54.106:1337/number/swap?number="+n, true);
-      xhttp.send(n.toString());
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          console.log(this.responseText);
-          changeNumberOnDom(JSON.parse(this.responseText).data);
-        }
-      };
-    }
-    loadDoc();
+    return number;
+  }
 
-    // old code
-    // var tempObj = {
-    //     "928-445-9233":"928-123-4567",
-    //     "(928)-123-4567":"(928)-198-7654",
-    //     "132-456-7890":"098-765-4321"
-    // }
+  removeSpecialCharFromNumber = function(number) {
+    return parseInt(number.toString().trim().replace(/[^\d\+]/g,""));
+  }
 
-
-    // function changeNumberOnDom(replaceNumberObj){
-    //     for(let key in replaceNumberObj){
-    //         document.body.innerHTML = document.body.innerHTML.split(key).join(replaceNumberObj[key]);
-    //     }
-    // }
-
-
-    
-
-
-    
-    // // allNumberPatterns();
-
-
-    // function loadDoc() {
-    //   console.log("update data");
-    //   changeNumberOnDom(tempObj); 
-    //   var xhttp = new XMLHttpRequest();
-    //   xhttp.onreadystatechange = function() {
-        
-    //     if (this.readyState == 4 && this.status == 200) {
-    //         console.log("this.responseText::",this.responseText);
-    //         // document.getElementById("replaceme").innerHTML = this.responseText;
-    //         changeNumberOnDom(tempObj); 
-    //     }
-    //   };
-      
-    //   xhttp.open("GET", "https://f3ad67de.ngrok.io/auth/sampleScrapApi?url="+window.location.href, true);
-      
-    //   xhttp.send();
-    // }
+  loadDoc = function() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "http://localhost:1337/number/swap?number="+getFilterNumberArray(n)+"&referrer_tracking_source="+getSource(document.referrer, document.URL), true);
+    xhttp.send(n.toString());
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        changeNumberOnDom(JSON.parse(this.responseText).data);
+      }
+    };
+  }
